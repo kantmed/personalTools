87116e6da7955068fe73858b85ec4a89475982aa
kantmoh <kantmoh@gmail.com>
2023-10-14 07:57:43 +0100

    Home 202310140757

--- services/helpers.py
+++ services/helpers.py
@@ -1,6 +1,6 @@
 import locale
 
-locale.setlocale(locale.LC_ALL,'')
+locale.setlocale(locale.LC_ALL,'fr_FR')
 
 def formatNumber(number):
     return locale.currency(number,grouping=True,symbol=False)
